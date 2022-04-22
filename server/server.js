fff46063e0a09c51029ba4af62cf12275a938892
Debbie <dbarros64@gmail.com>
2022-04-22 04:44:41 -0700

    Deploying project

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ app.use(checkToken);
 app.use("/api/users", users);
 app.use("/api/articles", articles);
 
+app.use(express.static('client/build'));
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index/html'))
+    })
+}
+
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
 
 
